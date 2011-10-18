5bb0b049857416665e372558e69a5a3a4b0f2ef6
David <tinystatemachine@gmail.com>
2011-10-18 18:20:43 -0300

    Adding lookup by vanity name and default to shell username
      * also, more spacing in output

--- rdio-blame.py
+++ rdio-blame.py
@@ -4,23 +4,32 @@ import sys
 import json
 import oauth2 as oauth
 import urllib
+import getpass
 
-RDIO_CONSUMER_KEY = '<CONSUMER_KEY>'
-RDIO_CONSUMER_SECRET = '<CONSUMER_SECRET>'
+RDIO_CONSUMER_KEY = ''
+RDIO_CONSUMER_SECRET = ''
 
-if sys.argv[1][0] is not "s":
-  user = "s" + sys.argv[1]
+if len(sys.argv) > 1:
+  username = sys.argv[1]
 else:
-  user = sys.argv[1]
+	username = getpass.getuser()
 
 consumer = oauth.Consumer(RDIO_CONSUMER_KEY, RDIO_CONSUMER_SECRET)
 client = oauth.Client(consumer)
-response = client.request('http://api.rdio.com/1/', 'POST', 
-  urllib.urlencode({'method': 'getHeavyRotation', 'user': user, 'friends': 1,  'extras': '-trackKeys, users', 'limit': 12}))
+
+user_response = client.request('http://api.rdio.com/1/', 'POST',
+  urllib.urlencode({'method': 'findUser', 'vanityName': username }))
+
+currentUser = json.loads(user_response[1])['result']
+
+print "\nHeavy Rotation for %s\n" % unicode(currentUser['firstName'] + " " +  currentUser['lastName'])
+
+response = client.request('http://api.rdio.com/1/', 'POST',
+  urllib.urlencode({'method': 'getHeavyRotation', 'user': currentUser['key'], 'friends': 1,  'extras': '-trackKeys, users', 'limit': 12}))
 hr = json.loads(response[1])['result']
 
 for album in hr:
-  print "%s - %s" %( album['artist'], album['name'] )
+  print "  %s - %s" %( album['artist'], album['name'] )
   for user in album['users']:
     print "\t Name: %s URL: http://rdio.com%s " % ( unicode(user['firstName'] + " " +  user['lastName']).ljust(20), user['url'])
-
+  print ""
