9a2eaaa7e3e46552d6a58f2b0fc2aa391340dc95
Jonathon Klobucar <jonathon.klobucar@rd.io>
2011-10-26 10:34:01 -0700

    The blame function no longer works, as API support was removed for users, but you can still view anyones heavy rotation

--- rdio-blame.py
+++ rdio-blame.py
@@ -17,19 +17,9 @@ else:
 consumer = oauth.Consumer(RDIO_CONSUMER_KEY, RDIO_CONSUMER_SECRET)
 client = oauth.Client(consumer)
 
-user_response = client.request('http://api.rdio.com/1/', 'POST',
-  urllib.urlencode({'method': 'findUser', 'vanityName': username }))
-
-currentUser = json.loads(user_response[1])['result']
-
-print "\nHeavy Rotation for %s\n" % unicode(currentUser['firstName'] + " " +  currentUser['lastName'])
-
 response = client.request('http://api.rdio.com/1/', 'POST',
-  urllib.urlencode({'method': 'getHeavyRotation', 'user': currentUser['key'], 'friends': 1,  'extras': '-trackKeys, users', 'limit': 12}))
+  urllib.urlencode({'method': 'getHeavyRotation', 'user': user, 'friends': 1,  'extras': '-trackKeys', 'limit': 12}))
 hr = json.loads(response[1])['result']
 
 for album in hr:
-  print "  %s - %s" %( album['artist'], album['name'] )
-  for user in album['users']:
-    print "\t Name: %s URL: http://rdio.com%s " % ( unicode(user['firstName'] + " " +  user['lastName']).ljust(20), user['url'])
-  print ""
+  print "%s - %s" %( album['artist'], album['name'] )
