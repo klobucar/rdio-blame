0f98d5d2d76b600e48dabec99923700a387140cd
David Taylor <davidtaylor@foursquare.com>
2011-11-15 13:07:17 -0500

    Bah! rdio killed the 'users' from albums in HR. Scan instead.

--- rdio-blame.py
+++ rdio-blame.py
@@ -4,7 +4,7 @@ import sys
 import json
 import oauth2 as oauth
 import urllib
-import getpass
+from collections import defaultdict
 
 RDIO_CONSUMER_KEY = ''
 RDIO_CONSUMER_SECRET = ''
@@ -17,16 +17,45 @@ else:
 consumer = oauth.Consumer(RDIO_CONSUMER_KEY, RDIO_CONSUMER_SECRET)
 client = oauth.Client(consumer)
 
-user_response = client.request('http://api.rdio.com/1/', 'POST',
-  urllib.urlencode({'method': 'findUser', 'vanityName': username }))
+def name(u):
+  return u['firstName'] + " " + u['lastName']
 
-currentUser = json.loads(user_response[1])['result']
+def hr(user):
+  response = client.request('http://api.rdio.com/1/', 'POST',
+    urllib.urlencode({'method': 'getHeavyRotation', 'user': user, 'friends': 1,  'extras': '-trackKeys', 'limit': 12}))
+  return json.loads(response[1])['result']
 
-response = client.request('http://api.rdio.com/1/', 'POST',
-  urllib.urlencode({'method': 'getHeavyRotation', 'user': currentUser['key'], 'friends': 1,  'extras': '-trackKeys', 'limit': 12}))
+def userFromVanity(name):
+  user_response = client.request('http://api.rdio.com/1/', 'POST',
+    urllib.urlencode({'method': 'findUser', 'vanityName': name }))
+  return json.loads(user_response[1])['result']
 
-hr = json.loads(response[1])['result']
+currentUser = userFromVanity(username)
 
-for album in hr:
-  print "%s - %s" %( album['artist'], album['name'] )
-  print "\t Score: %s" % (album['hits'])
+follows_response = client.request('http://api.rdio.com/1/', 'POST',
+  urllib.urlencode({'method': 'userFollowing', 'user': currentUser['key']}))
+
+follows = json.loads(follows_response[1])['result']
+
+friends = {}
+albums = defaultdict(list)
+
+print "\nHeavy Rotation for %s\n" % unicode(currentUser['firstName'] + " " + currentUser['lastName'])
+
+sys.stdout.write("Loading")
+sys.stdout.flush()
+
+for friend in follows:
+  friends[friend['key']] = friend
+  sys.stdout.write(".")
+  sys.stdout.flush()
+  for album in hr(friend['key']):
+    albums[album['key']].append(friend['key'])
+
+print ""
+
+for album in hr(currentUser['key']):
+  print " %s - %s" %( album['artist'], album['name'] )
+  for friendKey in albums[album['key']]:
+    print "   "+name(friends[friendKey])
+  print ""
